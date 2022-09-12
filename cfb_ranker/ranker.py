8219b02998f2dc4f5738f1072b990c1d4ca4a977
Josh <j01101111sh@gmail.com>
2022-09-12 14:29:06 -0500

    Initial commit

---
+++ cfb_ranker/ranker.py
@@ -0,0 +1,10 @@
+"""Module for obtaining ranking data
+"""
+
+import cfbd
+
+
+class Ranker:
+    """Class used to obtain ranking data based on parameters provided"""
+
+    api_config: cfbd.Configuration
