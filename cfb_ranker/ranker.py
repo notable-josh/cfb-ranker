ebd9890d81764729c78b006488f3268b631fbc2e
Josh <j01101111sh@gmail.com>
2022-09-12 16:02:22 -0500

    Added sample methods

--- cfb_ranker/ranker.py
+++ cfb_ranker/ranker.py
@@ -8,3 +8,32 @@ class Ranker:
     """Class used to obtain ranking data based on parameters provided"""
 
     api_config: cfbd.Configuration
+
+    def __init__(self, year: int, week: int):
+        pass
+
+    @property
+    def off_yds_wt(self) -> float:
+        """Weight assigned to total offensive yards
+
+        Returns:
+            float
+        """
+        return self.off_yds_wt
+
+    @off_yds_wt.setter
+    def off_yds_wt(self, value: float) -> None:
+        self.off_yds_wt = value
+
+    @property
+    def def_yds_wt(self) -> float:
+        """Weight assigned to total defensive yards
+
+        Returns:
+            float
+        """
+        return self.def_yds_wt
+
+    @def_yds_wt.setter
+    def def_yds_wt(self, value: float) -> None:
+        self.def_yds_wt = value
